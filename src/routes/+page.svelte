aeb49231a417535dc995c10fdc5833881e9a68a2
phillychi3 <phillychi3@gmail.com>
2024-04-28 22:54:14 +0800

    feat: add sidebar style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+    import Sidebar from "./Sidebar.svelte";
 </script>
 
 <svelte:head>
@@ -7,21 +8,28 @@
 </svelte:head>
 
 <section>
-	<h1 class="text-3xl font-bold underline">
-		wait...
-	</h1>
+
+    <Sidebar />
+    <h1 class="text-3xl font-bold underline w-[300px] flex-grow">
+        wait...
+    </h1>
+    <div class="w-[320px] flex-shrink">
+        <p class="text-lg">
+            這裡將會是一個探索頁面，用來展示最新上傳的資料庫。
+        </p>
+
+
+    </div>
 </section>
 
 <style>
 	section {
 		display: flex;
-		flex-direction: column;
+		flex-direction: row;
+        max-width: 1300px;
+        margin: 0 auto;
 		justify-content: center;
 		align-items: center;
 		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
 	}
 </style>
---
+++ src/routes/Sidebar.svelte
@@ -0,0 +1,36 @@
+<script>
+    import { _ } from 'svelte-i18n';
+</script>
+
+<div class="sidebar">
+    <div class="sort">
+        <button class="btn btn-outline btn-secondary">最新上傳</button>
+        <button class="btn btn-outline btn-secondary">最多投票</button>
+        <button class="btn btn-outline btn-secondary">最多下載</button>
+    </div>
+    <div class="tags">
+        <button class="btn">Normal</button>
+        <button class="btn">Normal</button>
+        <button class="btn">Normal</button>
+        <button class="btn">Normal</button>
+        <button class="btn">Normal</button>
+        <button class="btn">Normal</button>
+    </div>
+</div>
+
+<style>
+    .sidebar {
+        width: 200px;
+        overflow-y: scroll;
+    }
+    .sort {
+        display: flex;
+        flex-direction: column;
+        gap: 6px;
+    }
+    .tags {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+    }
+</style>
