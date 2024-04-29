31ebee257383822ac9eb899dd08a9d3ed0b2ff11
phillychi3 <phillychi3@gmail.com>
2024-04-30 00:13:39 +0800

    feat: add search input

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-    import Sidebar from "./Sidebar.svelte";
+	import Sidebar from './Sidebar.svelte';
 </script>
 
 <svelte:head>
@@ -8,48 +8,40 @@
 </svelte:head>
 
 <section>
-
-    <Sidebar />
-    <div class="mainshow">
-        <div class="search">
-            <input type="text" placeholder="搜尋" />
-            <button>搜尋</button>
+	<Sidebar />
+	<div class="mainshow px-4">
+        <div class="join">
+            <input type="text" placeholder="Type here" class="input input-accent input-bordered join-item w-full" />
+            <button class="btn btn-accent join-item ">Search</button>
         </div>
-        <div class="main flex flex-row">
-            <h1 class="text-3xl font-bold underline w-[300px] flex-grow">
-                wait...
-            </h1>
-            <div class="w-[320px] flex-shrink">
-                <p class="text-lg">
-                    這裡將會是一個探索頁面，用來展示最新上傳的資料庫。
-                </p>
-            </div>
-        </div>
-    </div>
+		<div class="main flex flex-row flex-grow">
+			<h1 class="text-3xl w-[300px] flex-grow">主</h1>
+			<div class="w-[320px] flex-shrink">
+				<p class="text-lg">這裡是廣告</p>
+			</div>
+		</div>
+	</div>
 </section>
 
 <style>
 	section {
 		display: flex;
 		flex-direction: row;
-        max-width: 1300px;
-        margin: 0 auto;
+		align-items: flex-start;
+		max-width: 1300px;
+		margin: 0 auto;
 		justify-content: center;
-		align-items: center;
 		flex: 0.6;
 	}
 
-    .mainshow {
-        width: 600px;
-        display: flex;
-        flex-direction: column;
-        flex-grow: 1;
-        align-items: flex-start;
-    }
+	.mainshow {
+		width: 600px;
+		display: flex;
+		flex-direction: column;
+		flex-grow: 1;
+	}
 
-
-    .main {
-        gap: 1rem;
-
-    }
+	.main {
+		gap: 1rem;
+	}
 </style>
