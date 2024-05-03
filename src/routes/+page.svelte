9cc97d755780bd93ddeeaa158e135fb73f25c381
phillychi3 <phillychi3@gmail.com>
2024-05-04 04:49:14 +0800

    feat: add card

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Sidebar from './Sidebar.svelte';
+    import Showcard from './showcard.svelte';
 </script>
 
 <svelte:head>
@@ -19,23 +20,34 @@
 			<button class="btn btn-accent join-item">Search</button>
 		</div>
 		<div class="main flex flex-row flex-grow mt-4">
-			<div class="w-[300px] flex-grow grid xl:grid-cols-2 grid-cols-1 gap-2 ">
-				<div>
-                    <h2 class="text-2xl">test text</h2>
-                </div>
-				<div>
-                    <h2 class="text-2xl">test text</h2>
-                </div>
-				<div>
-                    <h2 class="text-2xl">test text</h2>
-                </div>
-				<div>
-                    <h2 class="text-2xl">test text</h2>
-                </div>
+			<div class="w-[300px] flex-grow">
+				<div
+					class="flex items-center bg-[#500E38] radio-sm p-2 rounded-md mb-4 mt-4 flex-col"
+				>
+					<h1 class="text-4xl">探索</h1>
+				</div>
+				<div class="grid xl:grid-cols-2 grid-cols-1 gap-2">
+					<div>
+						<Showcard image="https://cdn.discordapp.com/attachments/1001472836625518612/1231553847323787276/mmexport1713695657329.png?ex=66360f66&is=6634bde6&hm=3ef01ccb59ff66a3e3ded4ae1df9c92b6f79905320df4d996e62279889b0f152&"/>
+					</div>
+					<div>
+						<h2 class="text-2xl">test text</h2>
+					</div>
+					<div>
+						<h2 class="text-2xl">test text</h2>
+					</div>
+					<div>
+						<h2 class="text-2xl">test text</h2>
+					</div>
+				</div>
 			</div>
 			<div class="infocards w-[280px] flex-shrink">
 				<p class="text-lg">這裡是廣告</p>
-                <img src="https://p2.bahamut.com.tw/S/2KU/56/39a4da01b592a7029dbd57530a1poho5.JPG" alt="廣告" class="w-full h-auto" />
+				<img
+					src="https://p2.bahamut.com.tw/S/2KU/98/e30d11a8a9f73a57a1eabce89a1pjyy5.JPG"
+					alt="廣告"
+					class="w-full h-auto"
+				/>
 			</div>
 		</div>
 	</div>
---
+++ src/routes/showcard.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    export let image: string = ""
+    export let tags: string[] = []
+    export let author: string[] = []
+    export let download: number = 0
+    export let vote: number = 0
+</script>
+
+<div class="flex flex-col items-center">
+    <div class="flex-1">
+        <img src={image} alt="Litematica" class="object-cover max-h-60" />
+    </div>
+    <div class="w-full">
+        <p>Tags: {tags.join(", ")}</p>
+    </div>
+    <div class="w-full">
+        <p>Author: {author.join(", ")}</p>
+    </div>
+    <div class="w-full">
+        <p>Download: {download}</p>
+    </div>
+    <div class="w-full">
+        <p>Vote: {vote}</p>
+    </div>
+</div>
