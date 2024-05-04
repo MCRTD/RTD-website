f4054158a447f16b27d3bdbbd44f4dea8c4a79cc
phillychi3 <phillychi3@gmail.com>
2024-05-05 03:32:07 +0800

    feat: add provacy and terms

--- src/routes/l/[slug]/+page.svelte
+++ src/routes/l/[slug]/+page.svelte
@@ -4,41 +4,47 @@
 
 <h1>{data.title}</h1>
 <div>{@html data.content}</div>
-<div class="carousel w-full">
-	<div id="item1" class="carousel-item w-full">
-		<img
+<div>
+    <div class="carousel w-full">
+        <div id="item1" class="carousel-item w-full">
+            <!-- svelte-ignore a11y-missing-attribute -->
+            <img
 			src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg"
 			class="w-full"
-		/>
-	</div>
-	<div id="item2" class="carousel-item w-full">
-		<img
+            />
+        </div>
+        <div id="item2" class="carousel-item w-full">
+            <!-- svelte-ignore a11y-missing-attribute -->
+            <img
 			src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg"
 			class="w-full"
-		/>
-	</div>
-	<div id="item3" class="carousel-item w-full">
-		<img
+            />
+        </div>
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <div id="item3" class="carousel-item w-full">
+            <img
 			src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg"
 			class="w-full"
-		/>
-	</div>
-	<div id="item4" class="carousel-item w-full">
-		<img
+            />
+        </div>
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <div id="item4" class="carousel-item w-full">
+            <img
 			src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg"
 			class="w-full"
-		/>
-	</div>
-</div>
-<div class="flex justify-center w-full py-2 gap-2">
-	<a href="#item1" class="btn btn-xs">1</a>
-	<a href="#item2" class="btn btn-xs">2</a>
-	<a href="#item3" class="btn btn-xs">3</a>
-	<a href="#item4" class="btn btn-xs">4</a>
+            />
+        </div>
+    </div>
+    <div class="flex justify-center w-full py-2 gap-2">
+        <a href="#item1" class="btn btn-xs">1</a>
+        <a href="#item2" class="btn btn-xs">2</a>
+        <a href="#item3" class="btn btn-xs">3</a>
+        <a href="#item4" class="btn btn-xs">4</a>
+    </div>
 </div>
 
-<div class="card">
-	<div class="card-body">
+    <div class="card">
+        <div class="card-body">
 		<ul class="menu menu-horizontal bg-base-200">
 			<li><a>Item 1</a></li>
 			<li><a>Item 2</a></li>
@@ -47,8 +53,13 @@
 	</div>
 </div>
 
-<div class="avatar">
-	<div class="w-16 rounded-full">
-		<img src="https://cdn.discordapp.com/avatars/762484891945664542/a3d0e4d30b78ce30a2ed22b51bf80df4.png?size=1024" />
-	</div>
+<div class="">
+    <div class="avatar">
+        <div class="w-16 rounded-full">
+            <img src="https://cdn.discordapp.com/avatars/762484891945664542/a3d0e4d30b78ce30a2ed22b51bf80df4.png?size=1024" />
+        </div>
+    </div>
+    <h2>
+        <a href="/u/1">User 1</a>
+    </h2>
 </div>
---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,4 @@
+<div class="container mx-auto px-4 py-4">
+    <p>本隱私條款將會因應需求修正內容，修正後的條約將會公布於伺服器以及網站</p>
+    <h2 class="h2">隱私權政策</h2>
+</div>
