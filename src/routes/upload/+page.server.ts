7e6985514841d88b4992580316ff449008c99ac6
phillychi3 <phillychi3@gmail.com>
2024-05-13 22:42:11 +0800

    feat: post litematica

---
+++ src/routes/upload/+page.server.ts
@@ -0,0 +1,54 @@
+import type { Actions } from './$types'
+import servername from '$lib/data'
+import { fail } from '@sveltejs/kit'
+
+export const actions = {
+	default: async (event) => {
+		const data = await event.request.formData()
+
+		const file = data.get('file') as File
+		const info = data.get('info') as string
+		const author = data.get('author') as string
+		// const server = data.get('server') as string
+		// const group = data.get('group') as string
+		const tags = data.get('tags') as string
+		const name = data.get('name') as string
+
+		const formData = new FormData()
+
+		formData.append('litematica', file)
+
+    const cookies = event.cookies.getAll();
+    let cookie = '';
+    for (const cookieObj of cookies) {
+      cookie += `${cookieObj.name}=${cookieObj.value}; `;
+    }
+		console.log(cookie)
+
+		const res = await fetch(servername + '/api/litematica', {
+			method: 'POST',
+			headers: {
+				Description: info,
+				GroupId: -1,
+				ServerId: -1,
+				Name: name,
+				Tags: tags,
+				Author: author,
+				cookie: cookie
+			},
+			body: formData
+		})
+		console.log(res)
+		if (!res.ok) {
+			const status = await res.json()
+			return fail(401, { error: status.detail })
+		}
+		const token = await res.json()
+		return {
+			status: 200,
+			body: {
+				token
+			}
+		}
+	}
+} satisfies Actions
--- src/routes/upload/+page.svelte
+++ src/routes/upload/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import Markdown from 'svelte-exmarkdown'
+	import { applyAction, enhance } from '$app/forms'
+	import toast, { Toaster } from 'svelte-french-toast'
+  import Tags from "svelte-tags-input"
 	let md = ''
+  let tags: string[]
 	let agreen = false
 	let cansubmit = (aagreen: Boolean) => {
 		if (aagreen) {
@@ -9,7 +13,7 @@
 		return false
 	}
 </script>
-
+<Toaster />
 <div class="hero min-h-screen bg-base-200">
 	<div class="hero-content flex-col-reverse lg:flex-row-reverse items-start">
 		<div class="text-center max-w-lg lg:text-left break-words">
@@ -17,64 +21,108 @@
 			<Markdown {md} />
 		</div>
 		<div class="card w-full max-w-lg bg-base-100 m-auto">
-			<form class="card-body">
+			<form
+				class="card-body"
+				enctype="multipart/form-data"
+				method="POST"
+				use:enhance={({ formData }) => {
+					formData.append('tags', tags.join(','))
+					return async ({ result }) => {
+						await applyAction(result)
+            if (result.type !== 'success') {
+              toast.error(result.data.error)
+              return
+            }
+            toast.success('Login successful')
+					}
+				}}
+			>
+      <div class="label">
+        <span class="label-text">名稱</span>
+      </div>
+      <label class="flex items-center gap-2">
+        <input
+          type="text"
+          name="name"
+          class="grow input input-bordered input-primary"
+          placeholder="tnt duper"
+        />
+      </label>
 				<div class="label">
-					<span class="label-text"
-						>檔案詳細檔案詳細檔案詳細檔案詳細檔案詳細檔案詳細檔案詳細檔案詳細檔案詳細</span
-					>
+					<span class="label-text">檔案</span>
 				</div>
 				<input
 					type="file"
+					name="file"
 					class="file-input file-input-bordered file-input-primary w-full max-w-xs"
 				/>
 				<div class="label">
-					<span class="label-text"
-						>詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細</span
-					>
+					<span class="label-text">關於作品的介紹，可以使用markdown</span>
 				</div>
-				<textarea class="textarea textarea-primary" placeholder="Info" bind:value={md}></textarea>
+				<textarea class="textarea textarea-primary" placeholder="Info" name="info" bind:value={md}
+				></textarea>
 				<div class="label">
-					<span class="label-text"
-						>詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細</span
-					>
+					<span class="label-text">作者的id 使用,分隔</span>
 				</div>
 				<label class="flex items-center gap-2">
 					<input
 						type="text"
+						name="author"
 						class="grow input input-bordered input-primary"
-						placeholder="其他作者"
+						placeholder="1,2"
 					/>
 				</label>
 				<div class="label">
-					<span class="label-text"
-						>詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細</span
-					>
+					<span class="label-text">伺服器ID (尚未完成)</span>
 				</div>
 				<label class="flex items-center gap-2">
 					<input
 						type="text"
 						class="grow input input-bordered input-primary"
 						placeholder="伺服器"
+						name="server"
 						disabled
 					/>
 				</label>
 				<div class="label">
-					<span class="label-text"
-						>詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細</span
-					>
+					<span class="label-text">群組ID (尚未完成)</span>
 				</div>
 				<label class="flex items-center gap-2">
 					<input
 						type="text"
 						class="grow input input-bordered input-primary"
 						placeholder="群組"
+						name="group"
 						disabled
 					/>
 				</label>
 				<div class="label">
-					<span class="label-text"
-						>詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細詳細</span
+					<span class="label-text">標籤</span>
+				</div>
+        <div class="input input-primary p-0 !important">
+          <div class="tags-input override">
+            <Tags
+              bind:tags={tags}
+              maxTags={3}
+              placeholder="Add Tag"
+            />
+          </div>
+        </div>
+				<div class="input input-bordered input-primary flex items-center gap-2">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 16 16"
+						fill="currentColor"
+						class="w-4 h-4 opacity-70"
+						><path
+							fill-rule="evenodd"
+							d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+							clip-rule="evenodd"
+						/></svg
 					>
+				</div>
+				<!-- <div class="label">
+					<span class="label-text">標籤</span>
 				</div>
 				<label class="input input-bordered input-primary flex items-center gap-2">
 					<input type="text" class="grow" placeholder="Add Tag" />
@@ -89,7 +137,7 @@
 							clip-rule="evenodd"
 						/></svg
 					>
-				</label>
+				</label> -->
 				<div class="form-control">
 					<label class="label cursor-pointer">
 						<span class="label-text">同意條款</span>
@@ -103,3 +151,21 @@
 		</div>
 	</div>
 </div>
+
+<style>
+	.tags-input.override :global(.svelte-tags-input-layout) {
+		background: none;
+    padding: 0%;
+    border: none;
+    border-color: #00000000;
+	}
+
+  .tags-input.override :global(.svelte-tags-input-layout:focus-within) {
+    outline: none;
+  }
+
+	/* .tags-input.override :global(.svelte-tags-input-tag) {
+		background-color: #00ff00;
+		border-radius: 5px;
+	} */
+</style>
