3db9435328be7bb649adc4907658ef48ea704f23
phillychi3 <phillychi3@gmail.com>
2024-05-14 21:56:57 +0800

    feat: file page

--- src/routes/l/[post]/(default)/file/+page.svelte
+++ src/routes/l/[post]/(default)/file/+page.svelte
@@ -1,1 +1,73 @@
-<h1 class="text-2xl font-bold">Inprocess...</h1>
+<script lang="ts">
+	import { browser } from '$app/environment'
+	export let data
+	function yymmddtostring(Datetime: string) {
+		var date = new Date(Datetime)
+		return (
+			date.getFullYear() +
+			'-' +
+			(date.getMonth() + 1) +
+			'-' +
+			date.getDate() +
+			' --  ' +
+			date.getHours() +
+			':' +
+			date.getMinutes()
+		)
+	}
+	function openInNewTab(url: string) {
+		if (!browser) return
+		const newWindow = window.open(url, '_blank')
+		if (newWindow) newWindow.focus()
+	}
+</script>
+
+<div class="bg-[oklch(var(--n))] rounded-xl p-2">
+	<div class="overflow-x-auto">
+		<table class="table table-zebra">
+			<!-- head -->
+			<thead>
+				<tr>
+					<th></th>
+					<th>Name</th>
+					<th>Release date</th>
+					<th>File Size</th>
+					<th></th>
+				</tr>
+			</thead>
+			<tbody>
+				{#each data.postdata.Files as item}
+					<tr>
+						<th></th>
+						<td>
+							<h2 class="text-sm font-bold">{item.FileName}</h2>
+						</td>
+						<td>
+							{yymmddtostring(item.ReleaseDate)}
+						</td>
+						{#if item.Size / 1204 > 1024}
+							<td>{(item.Size / 1024 / 1024).toFixed(2)} MB</td>
+						{:else}
+							<td>{(item.Size / 1024).toFixed(2)} KB</td>
+						{/if}
+						<th>
+							<button class="btn btn-success btn-outline btn-xs" on:click={() => openInNewTab(item.FilePath)}
+								>Download</button
+							>
+						</th>
+					</tr>
+				{/each}
+			</tbody>
+			<!-- foot -->
+			<tfoot>
+				<tr>
+					<th></th>
+					<th>Name</th>
+					<th>Release date</th>
+					<th>File Size</th>
+					<th></th>
+				</tr>
+			</tfoot>
+		</table>
+	</div>
+</div>
