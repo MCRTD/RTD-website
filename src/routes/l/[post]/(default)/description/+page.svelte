eb4ef65a822344fb1d156328bb52572049726867
phillychi3 <phillychi3@gmail.com>
2024-09-06 18:28:45 +0800

    fix: if no obj to show

--- src/routes/l/[post]/(default)/description/+page.svelte
+++ src/routes/l/[post]/(default)/description/+page.svelte
@@ -6,23 +6,24 @@
 	import { gfmPlugin } from 'svelte-exmarkdown/gfm'
 	import type { Plugin } from 'svelte-exmarkdown'
 	const plugins: Plugin[] = [
-    {
-      rehypePlugin: [rehypeHighlight]
+		{
+			rehypePlugin: [rehypeHighlight]
 		},
 		gfmPlugin()
 	]
 	export let data
-  let md = data.postdata.Description
+	let md = data.postdata.Description
 </script>
 
 <div class="bg-[oklch(var(--n))] rounded-xl p-2">
 	<div class="carousel w-full max-h-[500px]">
-		<div id="obj" class="carousel-item w-full">
-			<Canvas>
-				<Obj objdata={data.postdata.Files[0]} />
-			</Canvas>
-		</div>
-
+		{#if data.postdata.Files[0].LitematicaObj.ObjFilePath != ''}
+			<div id="obj" class="carousel-item w-full">
+				<Canvas>
+					<Obj objdata={data.postdata.Files[0]} />
+				</Canvas>
+			</div>
+		{/if}
 		<div id="item3" class="carousel-item w-full">
 			<img
 				src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg"
@@ -40,9 +41,14 @@
 		</div>
 	</div>
 	<div class="flex justify-center w-full py-2 gap-2">
-		<a href="#obj" class="btn btn-xs">1</a>
-		<a href="#item3" class="btn btn-xs">2</a>
-		<a href="#item4" class="btn btn-xs">3</a>
+		{#if data.postdata.Files[0].LitematicaObj.ObjFilePath != ''}
+			<a href="#obj" class="btn btn-xs">1</a>
+			<a href="#item3" class="btn btn-xs">2</a>
+			<a href="#item4" class="btn btn-xs">3</a>
+		{:else}
+			<a href="#item3" class="btn btn-xs">1</a>
+			<a href="#item4" class="btn btn-xs">2</a>
+		{/if}
 	</div>
 	<div class="markdown">
 		<Markdown {md} {plugins} />
