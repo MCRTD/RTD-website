b948f2bd927505487a2aece11b21300621f89004
phillychi3 <phillychi3@gmail.com>
2024-11-13 10:45:18 +0800

    fix: 錯誤頁面排版 用戶名稱顯

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,7 +5,7 @@
 <div>
 	<h1>It seems there has been an error, sorry about that.</h1>
 	{#if $page?.error}
-		<div class="mt-4 p-4 border-y-2">
+		<div class="mt-4 p-4">
 			{#if $page?.status}
 				<p>Page status: {$page?.status}</p>
 				<img src="https://http.cat/{$page?.status}" alt="HTTP status code" />
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -67,7 +67,7 @@
 									/>
 								</div>
 							</div>
-							<p>用戶名稱</p>
+							<p>{user.Username || "User"}</p>
 						</div>
 						<ul
 							class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
